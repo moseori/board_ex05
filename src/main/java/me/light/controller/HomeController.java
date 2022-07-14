8f1193d78399ebe8e2695a071b8151ee7965ae87
moseori <ksangheon@gmail.com>
2022-07-14 09:57:22 +0900

    2022-07-14 1. 기본설정(모듈 추가,기본 config)

---
+++ src/main/java/me/light/controller/HomeController.java
@@ -0,0 +1,14 @@
+package me.light.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String Home() {
+		return "home";
+	}
+
+}
