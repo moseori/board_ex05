623b640c1ec8785b20477bec91e9a6a6b1a5e95c
moseori <ksangheon@gmail.com>
2022-07-14 10:59:25 +0900

    2022-07-14 2. 데이터 베이스, log4, mapper 설정 및 테스트

---
+++ src/main/java/me/light/mapper/BoardMapper.java
@@ -0,0 +1,13 @@
+package me.light.mapper;
+
+import java.util.List;
+
+import me.light.model.BoardVO;
+
+public interface BoardMapper {
+List<BoardVO> getList();
+BoardVO get(Long bno);
+void insert(BoardVO vo);
+void update(BoardVO vo);
+void delete(Long bno);
+}
